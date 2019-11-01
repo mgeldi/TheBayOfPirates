822c15b0cc497a86b0a7fcc26b84d02ea5848c74
Rankhole <37880516+rankhole@users.noreply.github.com>
2019-11-01 13:26:19 +0100

    First commit

---
+++ src/main/java/de/htwberlin/de/TheBayOfPirates/TheBayOfPiratesApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.de.TheBayOfPirates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBayOfPiratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBayOfPiratesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/de/TheBayOfPirates/TheBayOfPiratesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.de.TheBayOfPirates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBayOfPiratesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
