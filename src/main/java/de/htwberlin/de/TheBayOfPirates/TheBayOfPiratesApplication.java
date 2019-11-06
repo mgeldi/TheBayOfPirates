16e5839e8674fd6808e8b589d4352687bdc5e8df
ali <ali.aslan@gmail.com>
2019-11-06 22:06:41 +0100

    - Es zeigt Tag und Zeit - Hab es mit Kommentaren versehen fürs verständnis (Controller & AboutUs) - Wir müssen dann noch die Tests machen und dann noch was nicht mit Coding zu hat

--- src/main/java/de/htwberlin/de/TheBayOfPirates/TheBayOfPiratesApplication.java
+++ src/main/java/de/htwberlin/de/TheBayOfPirates/TheBayOfPiratesApplication.java
@@ -2,6 +2,7 @@ package de.htwberlin.de.TheBayOfPirates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
