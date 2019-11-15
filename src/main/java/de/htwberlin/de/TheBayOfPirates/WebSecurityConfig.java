8407f908c2a81f5e760e10c4ef6537c086fb4644
Rankhole <muhammed_ali96@web.de>
2019-11-16 00:48:08 +0100

    Updated gitignore and permitted about page

--- src/main/java/de/htwberlin/de/TheBayOfPirates/WebSecurityConfig.java
+++ src/main/java/de/htwberlin/de/TheBayOfPirates/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home", "/about").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
