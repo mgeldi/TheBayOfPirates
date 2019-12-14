9513813ac9a7a5aca787de8370ee28a9cfb0cc59
Rankhole <muhammed_ali96@web.de>
2019-12-14 18:22:23 +0100

    Fixed logout button - it now works

--- src/main/java/de/htwberlin/de/TheBayOfPirates/config/WebSecurityConfig.java
+++ src/main/java/de/htwberlin/de/TheBayOfPirates/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -40,14 +41,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .permitAll()
                 .and()
-                .csrf().disable().formLogin()
+                .csrf().disable()
+                .formLogin()
                 .loginPage("/login")
                 .failureUrl("/?error=true")
                 .usernameParameter("email")
                 .passwordParameter("password")
                 .and()
-                .logout()
-                .logoutSuccessUrl("/")
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
                 .permitAll();
     }
 
