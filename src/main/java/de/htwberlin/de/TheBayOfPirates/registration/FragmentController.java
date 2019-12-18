209c6812ee754c55d3c260761ef3b61089714fda
aimazeme <aihong.ah@gmail.com>
2019-12-18 18:10:19 +0100

    thymeleaf fragment issue not fixed

---
+++ src/main/java/de/htwberlin/de/TheBayOfPirates/registration/FragmentController.java
@@ -0,0 +1,22 @@
+package de.htwberlin.de.TheBayOfPirates.registration;
+
+import de.htwberlin.de.TheBayOfPirates.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+
+@Controller
+public class FragmentController {
+
+    @GetMapping(value = "/navbar")
+    public ModelAndView getNavbar(Model model, Principal principal) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("fragments/navbar"); // resources/template/home.html
+        modelAndView.addObject("user", new User());
+        modelAndView.addObject("principal", principal);
+        return modelAndView;
+    }
+}
