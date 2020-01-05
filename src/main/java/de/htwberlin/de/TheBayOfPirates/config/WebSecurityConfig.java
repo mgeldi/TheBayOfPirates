73e40fb02ba562a9d9c5ca9b04b5a18a3610b70f
Rankhole <muhammed_ali96@web.de>
2020-01-05 14:45:36 +0100

    Fixed resource handler being blocked by @EnableWebMvc annotation

--- src/main/java/de/htwberlin/de/TheBayOfPirates/config/WebSecurityConfig.java
+++ src/main/java/de/htwberlin/de/TheBayOfPirates/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package de.htwberlin.de.TheBayOfPirates.config;
 
 import de.htwberlin.de.TheBayOfPirates.user.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,11 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -35,8 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "login", "/about", "/register").permitAll()
-                .antMatchers( "/search", "/torrent/*", "/profile/*").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/", "login", "/about", "/register", "/static/*", "/static/images/*").permitAll()
+                .antMatchers("/search", "/torrent/*", "/profile/*").hasAnyAuthority("USER", "ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
