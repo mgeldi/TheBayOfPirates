a8c7ae480dfcc8a71a7a908d33ef2086a16ed7de
Rankhole <muhammed_ali96@web.de>
2020-01-05 13:06:11 +0100

    Fixed some navbar stuff, still broken

--- src/main/java/de/htwberlin/de/TheBayOfPirates/registration/FragmentController.java
+++ src/main/java/de/htwberlin/de/TheBayOfPirates/registration/FragmentController.java
@@ -3,6 +3,7 @@ package de.htwberlin.de.TheBayOfPirates.registration;
 import de.htwberlin.de.TheBayOfPirates.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,6 +12,7 @@ import java.security.Principal;
 @Controller
 public class FragmentController {
 
+    @CrossOrigin
     @GetMapping(value = "/navbar")
     public ModelAndView getNavbar(Model model, Principal principal) {
         ModelAndView modelAndView = new ModelAndView();
