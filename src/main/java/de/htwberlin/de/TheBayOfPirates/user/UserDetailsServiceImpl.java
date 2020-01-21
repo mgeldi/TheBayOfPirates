41ec331ab2680e8f9c8d24f568e35ce0507bf2ff
Rankhole <muhammed_ali96@web.de>
2020-01-21 21:58:20 +0100

    Login now works with username or email

--- src/main/java/de/htwberlin/de/TheBayOfPirates/user/UserDetailsServiceImpl.java
+++ src/main/java/de/htwberlin/de/TheBayOfPirates/user/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package de.htwberlin.de.TheBayOfPirates.user;
 
 import de.htwberlin.de.TheBayOfPirates.role.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,9 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -29,22 +28,26 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> user = userService.findByUserEmail(username);
+        Optional<User> userWithEmail = userService.findByUserEmail(username);
+        Optional<User> userWithUsername = userService.findByUserName(username);
         UserBuilder builder = null;
-        if (user.isPresent()) {
-            builder = org.springframework.security.core.userdetails.User.withUsername(user.get().getEmail());
-            builder.password(user.get().getPassword());
-            Set grantedAuthorities = new HashSet<>();
-            for (Role role : user.get().getRoles()) {
-                grantedAuthorities.add(new SimpleGrantedAuthority(role.getRoleName()));
-                System.out.print(role.getRoleName());
-            }
-            builder.authorities(grantedAuthorities);
-
+        if (userWithEmail.isPresent()) {
+            return getBuilder(userWithEmail.get().getEmail(), userWithEmail.get().getPassword(), userWithEmail.get().getRoles()).build();
+        } else if (userWithUsername.isPresent()) {
+            return getBuilder(userWithUsername.get().getEmail(), userWithUsername.get().getPassword(), userWithUsername.get().getRoles()).build();
         } else {
             throw new UsernameNotFoundException("User not found");
         }
+    }
 
-        return builder.build();
+    private UserBuilder getBuilder(String email, String password, Set<Role> roles) {
+        UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(email);
+        builder.password(password);
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Role role : roles) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+        builder.authorities(grantedAuthorities);
+        return builder;
     }
 }
